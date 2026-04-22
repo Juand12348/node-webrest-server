7f2019096a337f21439aca852b49fa72c87054dd
Juan Martinez <juandavidmn0819@gmail.com>
2026-04-21 20:32:07 -0500

    Agregando funcionalidad RESTFULL API Todos

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,10 +1,11 @@
-import express from 'express';
+import express, { Router } from 'express';
 import path from 'node:path';
 
 
 interface Options {
 
     port: number;
+    routes: Router;
     public_path?: string;
 
 }
@@ -13,18 +14,31 @@ export class Server{
 
     private app = express();
     private readonly port: number;
+    private readonly routes: Router; 
     private readonly publicPath: string;
 
     constructor(options : Options){
-        const { port, public_path = 'public' } = options;
+        const { port, routes ,public_path = 'public' } = options;
         this.port = port;
         this.publicPath = public_path;
+        this.routes = routes;
     }
 
     async start(){
 
+        // Middlewares
+        this.app.use( express.json());
+        this.app.use( express.urlencoded({ extended: true })); // x-www-form-urlencoded
+
+        // Public Forlder
         this.app.use( express.static( this.publicPath));
 
+
+        // Routes
+        this.app.use(this.routes);
+
+
+        // SPA
         this.app.get('*', (req, res ) => {
             const indexPath = path.join(process.cwd(), this.publicPath, 'index.html');
             res.sendFile(indexPath);
