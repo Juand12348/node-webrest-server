ee3ca1583a3d5972fee4a0315969e97b0f22e597
Juan Martinez <juandavidmn0819@gmail.com>
2026-04-28 21:33:58 -0500

    Agregando arquitectura limpia

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,4 +1,5 @@
 import express, { Router } from 'express';
+import compression from 'compression';
 import path from 'node:path';
 
 
@@ -29,6 +30,7 @@ export class Server{
         // Middlewares
         this.app.use( express.json());
         this.app.use( express.urlencoded({ extended: true })); // x-www-form-urlencoded
+        this.app.use(compression());
 
         // Public Forlder
         this.app.use( express.static( this.publicPath));
